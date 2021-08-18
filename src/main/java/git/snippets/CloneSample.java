01e93e8ce834aeebaca013e46fe4c783a2d647ab
GreyZeng <410486047@qq.com>
2021-08-18 23:34:31 +0800

    object clone

--- src/main/java/git/snippets/CloneSample.java
+++ src/main/java/git/snippets/CloneSample.java
@@ -10,11 +10,11 @@ import java.io.Serializable;
 // 克隆方法1 实现Cloneable接口
 // 克隆方法2 实现Serializable接口，序列化反序列化
 
-class DemoInternal2  implements Serializable {
+class DemoInternal2 implements Serializable {
   private String internalName;
 
   private String internalValue;
-  
+
   public String getInternalName() {
     return internalName;
   }
@@ -35,65 +35,70 @@ class DemoInternal2  implements Serializable {
    * 
    */
   private static final long serialVersionUID = -3683926169721761903L;
-  
+
 }
+
+
 class Address implements Cloneable {
-    private String add;
+  private String add;
 
-    public String getAdd() {
-        return add;
-    }
+  public String getAdd() {
+    return add;
+  }
 
-    public void setAdd(String add) {
-        this.add = add;
-    }
+  public void setAdd(String add) {
+    this.add = add;
+  }
 
-    @Override
-    public Object clone() {
-        Address addr = null;
-        try{
-            addr = (Address)super.clone();
-        }catch(CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
-        return addr;
+  @Override
+  public Object clone() {
+    Address addr = null;
+    try {
+      addr = (Address) super.clone();
+    } catch (CloneNotSupportedException e) {
+      e.printStackTrace();
     }
+    return addr;
+  }
 }
 
-class Student implements Cloneable{
-    private int number;
 
-    private Address addr;
+class Student implements Cloneable {
+  private int number;
 
-    public Address getAddr() {
-        return addr;
+  private Address addr;
+
+  public Address getAddr() {
+    return addr;
+  }
+
+  public void setAddr(Address addr) {
+    this.addr = addr;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  @Override
+  public Object clone() {
+    Student stu = null;
+    try {
+      stu = (Student) super.clone(); // 浅复制
+    } catch (CloneNotSupportedException e) {
+      e.printStackTrace();
     }
-
-    public void setAddr(Address addr) {
-        this.addr = addr;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    @Override
-    public Object clone() {
-        Student stu = null;
-        try{
-            stu = (Student)super.clone();   //浅复制
-        }catch(CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
-        stu.addr = (Address)addr.clone();   //深度复制
-        return stu;
-    }
+    stu.addr = (Address) addr.clone(); // 深度复制
+    return stu;
+  }
 }
-public class CloneSample  implements Serializable {
+
+
+public class CloneSample implements Serializable {
   /**
    * 
    */
@@ -104,8 +109,8 @@ public class CloneSample  implements Serializable {
   private String value;
 
   private DemoInternal2 demoInternal2;
-  
-    public String getName() {
+
+  public String getName() {
     return name;
   }
 
@@ -128,38 +133,40 @@ public class CloneSample  implements Serializable {
   public void setDemoInternal2(DemoInternal2 demoInternal2) {
     this.demoInternal2 = demoInternal2;
   }
+
   public CloneSample myclone() {
     CloneSample demo2 = null;
     try {
-        // 写入字节流
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(baos);
-        oos.writeObject(this);
+      // 写入字节流
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      ObjectOutputStream oos = new ObjectOutputStream(baos);
+      oos.writeObject(this);
 
-        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-        ObjectInputStream ois = new ObjectInputStream(bais);
-        demo2 = (CloneSample) ois.readObject();
+      ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+      ObjectInputStream ois = new ObjectInputStream(bais);
+      demo2 = (CloneSample) ois.readObject();
     } catch (IOException | ClassNotFoundException e) {
-        e.printStackTrace();
+      e.printStackTrace();
     }
     return demo2;
-} 
-    public static void main(String args[]) {
+  }
 
-        Address addr = new Address();
-        addr.setAdd("杭州市");
-        Student stu1 = new Student();
-        stu1.setNumber(123);
-        stu1.setAddr(addr);
+  public static void main(String args[]) {
 
-        Student stu2 = (Student)stu1.clone();
+    Address addr = new Address();
+    addr.setAdd("杭州市");
+    Student stu1 = new Student();
+    stu1.setNumber(123);
+    stu1.setAddr(addr);
 
-        System.out.println("学生1:" + stu1.getNumber() + ",地址:" + stu1.getAddr().getAdd());
-        System.out.println("学生2:" + stu2.getNumber() + ",地址:" + stu2.getAddr().getAdd());
+    Student stu2 = (Student) stu1.clone();
 
-        addr.setAdd("西湖区");
+    System.out.println("学生1:" + stu1.getNumber() + ",地址:" + stu1.getAddr().getAdd());
+    System.out.println("学生2:" + stu2.getNumber() + ",地址:" + stu2.getAddr().getAdd());
 
-        System.out.println("学生1:" + stu1.getNumber() + ",地址:" + stu1.getAddr().getAdd());
-        System.out.println("学生2:" + stu2.getNumber() + ",地址:" + stu2.getAddr().getAdd());
-    }
+    addr.setAdd("西湖区");
+
+    System.out.println("学生1:" + stu1.getNumber() + ",地址:" + stu1.getAddr().getAdd());
+    System.out.println("学生2:" + stu2.getNumber() + ",地址:" + stu2.getAddr().getAdd());
+  }
 }
