d28337e2cc59c42c73d3719c44d67117fb8708ea
GreyZeng <410486047@qq.com>
2021-11-25 07:48:07 +0800

    update

--- src/main/java/git/snippets/jdk8/FunctionInterfaceDemo.java
+++ src/main/java/git/snippets/jdk8/FunctionInterfaceDemo.java
@@ -1,5 +1,7 @@
 package git.snippets.jdk8;
 
+import java.util.function.Function;
+
 /**
  * @author <a href="mailto:410486047@qq.com">Grey</a>
  * @date 2021/11/20
@@ -7,8 +9,34 @@ package git.snippets.jdk8;
  */
 public class FunctionInterfaceDemo {
     public static void main(String[] args) {
+        func0();
+        func1();
+        func3();
+        func4();
+    }
+
+    static void func0() {
         FunctionDemo demo = (name, age) -> System.out.println("我叫" + name + "我今年" + age + "岁");
         demo.say("zhangsan", 20);
         demo.hi("zhanshang", 20);
+    }
+
+    static void func1() {
+        Function<String, String> toUpperCase = (String::toUpperCase);
+        String apply = toUpperCase.apply("hello,world");
+        System.out.println(apply);
+    }
+
+    static void func3() {
+        Function<String, Integer> lengthFunction = str -> str.length();
+        Integer length = lengthFunction.apply("www.wdbyte.com");
+        System.out.println(length);
+    }
+
+    static void func4() {
+        Function<String, Integer> lengthFunction = str -> str.length();
+        Function<Integer, Integer> doubleFunction = length -> length * 2;
+        Integer doubleLength = lengthFunction.andThen(doubleFunction).apply("hello,world");
+        System.out.println(doubleLength);
     }
 }
