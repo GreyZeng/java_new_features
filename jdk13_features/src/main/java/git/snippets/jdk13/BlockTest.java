d7d51e4ca03a59f31a9a09b7dc2e23292e38d98a
zenghui <410486047@qq.com>
2021-11-29 14:33:28 +0800

    update

---
+++ jdk13_features/src/main/java/git/snippets/jdk13/BlockTest.java
@@ -0,0 +1,26 @@
+package git.snippets.jdk13;
+
+/**
+ * 文本块
+ * 在jdk13是预览功能
+ * 到jdk15可正式使用
+ *
+ * @author <a href="mailto:410486047@qq.com">Grey</a>
+ * @date 2021/11/29
+ * @since 13
+ */
+public class BlockTest {
+//    public static void main(String[] args) {
+//        blockTest();
+//    }
+//
+//    private static void blockTest() {
+//        String content2 = """
+//                {
+//                    1:"Hello",
+//                    2:"World"
+//                }
+//                """;
+//        System.out.println(content2);
+//    }
+}
---
+++ jdk13_features/src/main/java/git/snippets/jdk13/SwitchYield.java
@@ -0,0 +1,31 @@
+package git.snippets.jdk13;
+
+/**
+ * switch yield功能
+ * jdk13 实验性功能
+ * 到jdk14 正式使用
+ *
+ * @author <a href="mailto:410486047@qq.com">Grey</a>
+ * @date 2021/11/29
+ * @since 13
+ */
+public class SwitchYield {
+//    public static void main(String[] args) {
+//        String t = test("apple");
+//        System.out.println(t);
+//        String m = test("abc");
+//        System.out.println(m);
+//    }
+//
+//    private static String test(String c) {
+//        return switch (c) {
+//            case "apple", "Apple":
+//                yield "苹果";
+//            case "banana":
+//                yield "香蕉";
+//            default:
+//                yield "无法识别";
+//        };
+//    }
+
+}
