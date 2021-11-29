c05b18eb3ed840fcacbdf8f07f086272babea642
zenghui <410486047@qq.com>
2021-11-29 10:46:22 +0800

    update

---
+++ jdk11_features/src/main/java/git/snippets/jdk11/StringNewAPI.java
@@ -0,0 +1,48 @@
+package git.snippets.jdk11;
+
+/**
+ * String 新增API
+ *
+ * @author <a href="mailto:410486047@qq.com">Grey</a>
+ * @date 2021/11/29
+ * @see String
+ * @since
+ */
+public class StringNewAPI {
+    public static void main(String[] args) {
+        isBlankTest();
+        linesTest();
+        repeatTest();
+        stripTest();
+    }
+
+    private static void linesTest() {
+        String abcd = "\na\nb\tc\td";
+        abcd.lines().forEach(System.out::println);
+    }
+
+    private static void isBlankTest() {
+        String blank = "   ";
+        System.out.println(blank.isBlank());
+    }
+
+    static void repeatTest() {
+        String repeat = "abcd";
+        // repeat(n) 表示复制几次
+        String repeat3 = repeat.repeat(3);
+        System.out.println(repeat3);
+    }
+
+    // 去掉前后端的空白字符
+    static void stripTest() {
+        String a = "   bbbd a  ";
+        String b = a.strip();
+        System.out.println(b);
+        System.out.println(a);
+        String strip = "    我是       ";
+        // 去前面空白字符，如全角空格，TAB
+        System.out.println("==" + strip.stripLeading() + "==");
+// 去后面空白字符，如全角空格，TAB
+        System.out.println("==" + strip.stripTrailing() + "==");
+    }
+}
