e83b459e711ff04211d7ec60d880f94def6c66a5
GreyZeng <410486047@qq.com>
2022-06-27 12:56:40 +0800

    jdk18 new features

---
+++ jdk18_features/src/main/java/git/snippets/jdk18/UFT8Test.java
@@ -0,0 +1,10 @@
+package git.snippets.jdk18;
+
+import java.nio.charset.Charset;
+
+public class UFT8Test {
+    public static void main(String[] args) {
+        System.out.println(Charset.defaultCharset());
+        System.out.println("你好");
+    }
+}
