c7b0c513ae13b2cf014e802d17d6d9a00733e1cd
GreyZeng <410486047@qq.com>
2022-08-21 17:55:09 +0800

    update notes

--- jdk18_features/src/main/java/git/snippets/jdk18/UFT8Test.java
+++ jdk18_features/src/main/java/git/snippets/jdk18/UFT8Test.java
@@ -4,6 +4,9 @@ import java.nio.charset.Charset;
 
 public class UFT8Test {
     public static void main(String[] args) {
+        System.out.println("Default charset : " + Charset.defaultCharset());
+        System.out.println("file.encoding   : " + System.getProperty("file.encoding"));
+        System.out.println("native.encoding : " + System.getProperty("native.encoding"));
         System.out.println(Charset.defaultCharset());
         System.out.println("你好");
     }
