42994d6c4ede566b44f261a38656618b0987f1a5
GreyZeng <410486047@qq.com>
2024-09-08 21:13:12 +0800

    update code

--- snippets/src/main/java/git/snippets/core_java_books/Main.java
+++ snippets/src/main/java/git/snippets/core_java_books/Main.java
@@ -7,5 +7,17 @@ public class Main {
         System.out.println("Hello world!");
         var generator = new Random();
         System.out.println(generator.nextDouble());
+        Runnable task = () -> {
+            for (int i = 0; i < 10; i++) {
+                doWork();
+            }
+        };
+        task.run();
     }
+
+    private void doWork() {
+        System.out.println("Hello world!");
+    }
+
+
 }
